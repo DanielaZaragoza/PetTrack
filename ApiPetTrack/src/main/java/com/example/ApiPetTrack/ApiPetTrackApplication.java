cd6a1eb2e79ab63c8c733cf2eebbd21f8c6fb4ef
yered's <yeredvargas5@gmail.com>
2025-04-23 16:32:31 -0600

    test1

---
+++ ApiPetTrack/src/main/java/com/example/ApiPetTrack/ApiPetTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApiPetTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPetTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPetTrackApplication.class, args);
+	}
+
+}
---
+++ ApiPetTrack/src/test/java/com/example/ApiPetTrack/ApiPetTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiPetTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPetTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
